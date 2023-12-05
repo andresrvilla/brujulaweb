c6ca02616b4973bf478d7425d5646a191f2020f9
Andrés Villa <robertoavilla@gmail.com>
2023-12-05 17:17:45 -0300

    Add React Frontend

---
+++ brujulaweb/application/src/frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="imgs/128.png" alt="logo" />
+        <p>
+          Brujula Web
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- brujulaweb/application/src/main/java/net/brujulaweb/server/BrujulaWebServer.java
+++ brujulaweb/application/src/main/java/net/brujulaweb/server/BrujulaWebServer.java
@@ -1,11 +1,19 @@
 package net.brujulaweb.server;
 
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 
 public class BrujulaWebServer {
     public static void main(String[] args) {
-        var app = Javalin.create(/*config*/)
-                .get("/", ctx -> ctx.result("Hello World"))
-                .start(7070);
+        var app = Javalin.create(javalinConfig ->
+                {
+                    javalinConfig.staticFiles.add("/app");
+                    javalinConfig.spaRoot.addFile("/", "/app/index.html");
+                });
+                //.get("/", ctx -> ctx.result("Hello World"));
+
+
+
+        app.start(8080);
     }
 }
